43e84365a3c0c0cf97d6fe3a27ab518322704bfb
Jivesh21 <jiveshsharma2220@gmail.com>
2026-08-09 15:27:16 +0530

    serverjs updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 dotenv.config({
   path: "./.env",
 });
+
 console.log("NODE_ENV =", process.env.NODE_ENV);
+
 const { default: app } = await import("./app.js");
 const { default: connectDB } = await import("./config/db.js");
 
@@ -14,7 +16,7 @@ const startServer = async () => {
     await connectDB();
 
     app.listen(PORT, () => {
-      console.log(`🚀 Server running on http://localhost:${PORT}`);
+      console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Server startup failed:", error);
